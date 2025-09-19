61403cc20f6f1c15f700a155b80f9191775405d7
ZiqingHo <46319875+ZiqingHo@users.noreply.github.com>
2025-09-19 20:11:27 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const VERSION = 'v1.0.0';
+const CACHE_NAME = `llsk-gacha-${VERSION}`;
+const ASSETS = [
+  './',
+  './gacha_record.html',
+  './manifest.webmanifest',
+  './service-worker.js',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req).then((res) => {
+        if (req.method === 'GET' && res && res.status === 200 && res.type === 'basic') {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone));
+        }
+        return res;
+      }).catch(() => caches.match('./gacha_record.html'));
+    })
+  );
+});
